bb757a28f68aa688699d522ef43bb16ae41d2124
Alitor004 <irmaosgomes1302@gmail.com>
2024-03-19 12:47:26 -0300

    TipsCalculator

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button15Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPercentageLabel.Text = $"{PercentageSlider.Value.toString()}%";
+        }
+    }
+
+}
